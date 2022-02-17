a39b577062432a53a59c991eeee2e2dc0e919180
Yahya <yaya.ly@uadb.edu.sn>
2022-02-17 13:41:56 +0000

    deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
- app.use(express.static('./dist/panierClient'));
+ app.use(express.static(__dirname+'./dist/panierClient'));
 
 app.get('/*', (req, res) =>{
-    res.sendFile('index.html', {root: 'dist/panierClient/'});
+    res.sendFile(path.join(__dirname,'dist/panierClient/'));
 
 
 },); 
