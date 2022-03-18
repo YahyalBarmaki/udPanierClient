4c55f8c4064d5a6a07340dacc9309480249b80a2
YahyalBarmaki <yaya.ly@uadb.edu.sn>
2022-03-18 09:06:37 +0100

    sauvegarde

--- src/app/components/commandes/commandes.component.ts
+++ src/app/components/commandes/commandes.component.ts
@@ -63,6 +63,8 @@ export class CommandesComponent implements OnInit {
     this.numbetItem();
     this.cartFunction();
     console.log(this.getTotal());
+    //console.log(this.payForm);
+
     console.log(this.payForm);
     this.payForm.get('invoice.total_amount')?.setValue(this.getTotal());
   }
