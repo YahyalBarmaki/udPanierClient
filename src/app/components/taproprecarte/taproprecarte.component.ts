9b81376be01c16d271b196f7296508f70aa55164
YahyalBarmaki <yaya.ly@uadb.edu.sn>
2022-03-23 10:03:50 +0100

    design your own card

--- src/app/components/taproprecarte/taproprecarte.component.ts
+++ src/app/components/taproprecarte/taproprecarte.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./taproprecarte.component.css']
 })
 export class TaproprecarteComponent implements OnInit {
-
+  
+  isProfile = false
+  isTel = false
+  isweb = false
+  isAddr = false
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  clickProfile(){
+    this.isProfile = !this.isProfile
+  }
+  clickisTel(){
+    this.isTel = !this.isTel
+  }
+  clickisweb(){
+    this.isweb = !this.isweb
+  }
+  clickisAddr(){
+    this.isAddr = !this.isAddr
+  }
 }
